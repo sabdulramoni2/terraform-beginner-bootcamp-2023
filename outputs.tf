09cec99b55b6d63b800a9e3c6e2a8db5006fd3af
saidi abdulramoni <abdulramonisaidi@gmail.com>
2023-10-10 05:49:52 +0000

    #40 implement  terraform provider block

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
-output "bucket" {
-    description = "Bucket name for our static website hosting"
-    value = module.terrahouse_aws.bucket_name
-}
+#output "bucket" {
+#    description = "Bucket name for our static website hosting"
+#    value = module.terrahouse_aws.bucket_name
+#}
 
-output "S3_website_endpoint" {
-    description = "S3 Static Website hosting endpoint"
-    value = module.terrahouse_aws.website_endpoint
-}
+#output "S3_website_endpoint" {
+#    description = "S3 Static Website hosting endpoint"
+#    value = module.terrahouse_aws.website_endpoint
+#}
 
 
-output "cloudfront_url" {
-    description = "The Cloufront Distribution Domain Name"
-    value = module.terrahouse_aws.cloudfront_url
+#output "cloudfront_url" {
+#    description = "The Cloufront Distribution Domain Name"
+#    value = module.terrahouse_aws.cloudfront_url
   
-}
+#}
