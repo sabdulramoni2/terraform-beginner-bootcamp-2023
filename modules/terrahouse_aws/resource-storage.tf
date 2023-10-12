4af8cfb82a34eebe8856c3c9d9a6d98b9d27bd91
saidi abdulramoni <abdulramonisaidi@gmail.com>
2023-10-12 19:20:30 +0000

    #45 test our terrahome on terratowns test town

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -1,16 +1,16 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
- resource "aws_s3_bucket" "website_bucket" {
+resource "aws_s3_bucket" "website_bucket" {
   # Bucket Naming Rules
-  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = var.bucket_name
-
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  # we want to assign a random bucket name
+  #bucket = var.bucket_name
 
   tags = {
-    UserUuid        = var.user_uuid
+    UserUuid = var.user_uuid
   }
-
 }
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration#website_endpoint
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
 resource "aws_s3_bucket_website_configuration" "website_configuration" {
   bucket = aws_s3_bucket.website_bucket.bucket
 
@@ -22,76 +22,69 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
     key = "error.html"
   }
 }
-
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-
-resource "aws_s3_object" "index" {
+resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
   source = var.index_html_filepath
   content_type = "text/html"
+
   etag = filemd5(var.index_html_filepath)
   lifecycle {
-    replace_triggered_by = [ terraform_data.content_version.output]
-    ignore_changes = [ etag ]
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
   }
 }
 
-
 resource "aws_s3_object" "upload_assets" {
-  for_each = fileset(var.assets_path,"*.{jpg,png}")
+  for_each = fileset(var.assets_path,"*.{jpg,png,gif}")
   bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
+  key    = "assets/${each.key}"
   source = "${var.assets_path}/${each.key}"
   etag = filemd5("${var.assets_path}${each.key}")
   lifecycle {
-    replace_triggered_by = [ terraform_data.content_version.output]
-    ignore_changes = [ etag ]
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
   }
-  
 }
-#fileset("${path.root}/public/assets","*.{jpg,png}")
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-
-resource "aws_s3_object" "error" {
+resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
   source = var.error_html_filepath
-  content_type = "error/html"
+  content_type = "text/html"
+
   etag = filemd5(var.error_html_filepath)
   #lifecycle {
-   # ignore_changes = [ etag ]
+  #  ignore_changes = [etag]
   #}
 }
-
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.bucket
   #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
   policy = jsonencode({
     "Version" = "2012-10-17",
-    "Statement"= {
-            "Sid"= "AllowCloudFrontServicePrincipalReadOnly",
-            "Effect"= "Allow",
-            "Principal"= {
-                "Service" = "cloudfront.amazonaws.com"
-            },
-            "Action"=  "s3:GetObject",
-            "Resource" = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
-            "Condition" = {
-            "StringEquals" = {
-             "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
-             #"AWS:SourceArn": data.aws_caller_identity.current.arn
-                }
-            }
+    "Statement" = {
+      "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
+      "Effect" = "Allow",
+      "Principal" = {
+        "Service" = "cloudfront.amazonaws.com"
+      },
+      "Action" = "s3:GetObject",
+      "Resource" = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
+      "Condition" = {
+      "StringEquals" = {
+          #"AWS:SourceArn": data.aws_caller_identity.current.arn
+          "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
         }
-  })       
+      }
+    }
+  })
 }
+
 
 resource "terraform_data" "content_version" {
   input = var.content_version
-  
 }
-
-
